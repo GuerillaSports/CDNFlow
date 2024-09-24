a498b929400b08d0c760b2b3e0d0a539086bdde1
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2024-09-24 12:54:36 -0600

    ADDS: functionality

--- src/global/color/color-contrast.ts
+++ src/global/color/color-contrast.ts
@@ -1,11 +1,32 @@
+import { GS_BLUE } from "../consts/colors";
+
 export function manageTitleColorContrast(card: HTMLElement | null) {
   if (!card) return
-  const gs_blue = getComputedStyle(card).getPropertyValue('--blue')
   const on_color = card.querySelector(".title-on-color") as HTMLElement | null
-  if (!on_color) return;
+  const target = card.querySelector(".title-blue") as HTMLElement | null
+  updateTitleColor(target, on_color)
+  // if (!on_color) return;
+  // if (on_color.style.color === "white") {
+  //   const story_title = card.querySelector(".title-blue") as HTMLElement | null
+  //   // if (!story_title) return
+  //   console.log(story_title, !!story_title)
+  //   if (story_title === null) return
+  //   story_title.style.color = GS_BLUE;
+  // }
+}
+
+export function updateTitleColor(target: HTMLElement | null, on_color: HTMLElement | null) {
+  if (!target || !on_color) return
   if (on_color.style.color === "white") {
-    const story_title = card.querySelector(".title-blue") as HTMLElement | null
-    if (!story_title) return
-    story_title.style.color = gs_blue;
+    target.style.color = GS_BLUE
+  }
+
+}
+
+export function manageColorContrast() {
+  console.log("[gs-cdnflow/global]: managing title color contrast...")
+  const cards = document.querySelectorAll(".card-label") as NodeListOf<HTMLElement>;
+  for (let card of cards) {
+    manageTitleColorContrast(card)
   }
 }
--- src/global/functions.ts
+++ src/global/functions.ts
@@ -1,40 +1,34 @@
-import { manageTitleColorContrast } from "./color/color-contrast";
+import { manageColorContrast, manageTitleColorContrast } from "./color/color-contrast";
+import { format_custom_story_title, manage_custom_story_title_instances } from "./titles/manageCustomTitles";
 
-function manageColorContrast() {
-  console.log("[gs-cdnflow/global]: managing title color contrast...")
-  const cards = document.querySelectorAll(".card-label") as NodeListOf<HTMLElement>;
-  for (let card of cards) {
-    manageTitleColorContrast(card)
-  }
-}
-
-
-function observeStoryTitleChanges() {
+function mange_dynamic_list_content() {
   const dynamic_list = document.querySelector(".dynam-list")
   if (!dynamic_list) return
-
   const config = { childList: true, subtree: true }
-  const story_title_observer = new MutationObserver((mutList) => {
+  const dynamic_list_content_observer = new MutationObserver((mutList) => {
     for (let mut of mutList) {
       // check if element has been added
       if (mut.addedNodes.length) {
         for (let card of mut.addedNodes) {
-          console.log("[gs-cdnflow/global]: MutationObeserver detected new content. Managing title color contrast...")
-          manageTitleColorContrast(card as HTMLElement | null)
+          console.log("[gs-cdnflow/global]: MutationObeserver detected new content. Managing...")
+          if (!card) continue
+          format_custom_story_title(card as HTMLElement) // FIRST
+          manageTitleColorContrast(card as HTMLElement)
         }
       }
     }
   })
-
-  story_title_observer.observe(dynamic_list, config)
+  dynamic_list_content_observer.observe(dynamic_list, config)
 }
 
 const globalFunctions: Record<string, any> = {
   DOMContentLoaded: {
+    manage_custom_story_title_instances, // FIRST (injexts content into what will be managed below)
     manageColorContrast,
+
   },
   load: {
-    observeStoryTitleChanges
+    mange_dynamic_list_content,
   }
 }
 
